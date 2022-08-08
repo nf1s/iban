2c8f7a03fb5aed9473c0a37674f42f63962ed294
Ahmed Nafies <ahmed.nafies@gmail.com>
2022-08-08 19:39:10 +0200

    refactor: adds utilities used by the rules engine conditions

---
+++ utils.py
@@ -0,0 +1,13 @@
+from consts import char_to_num
+
+
+def convert_iban_to_num(iban: str) -> int:
+    return int("".join(char_to_num[char] for char in iban))
+
+
+def move_the_1st_4_char_to_the_end(iban: str) -> str:
+    return iban[4:] + iban[:4]
+
+
+def mod_97(num: int) -> int:
+    return num % 97
