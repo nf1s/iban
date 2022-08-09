5fe198ddcb11dc60072c13452ef2a532dee38583
Ahmed Nafies <ahmed.nafies@gmail.com>
2022-08-09 16:21:30 +0200

    enhancement: improves error response messages

--- app/rules/actions.py
+++ app/rules/actions.py
@@ -1,9 +1,6 @@
-from app.exceptions import (
-    IbanDoesNotMatchCountryFormatError,
-    IbanLengthError,
-    Mod97CheckError,
-    NotAlphaNumericError,
-)
+from app.exceptions import (IbanDoesNotMatchCountryFormatError,
+                            IbanLengthError, Mod97CheckError,
+                            NotAlphaNumericError)
 from app.models import IbanModel
 
 
@@ -16,10 +13,17 @@ def raise_mod_97_check_error(iban: IbanModel):
 
 
 def raise_iban_length_does_not_match_country_length_error(iban: IbanModel):
-    raise IbanLengthError(iban, "IBAN length is not valid for the specified country")
+    raise IbanLengthError(
+        iban,
+        "IBAN length is not valid for the specified country, "
+        f"country={iban.country}, size={iban.size}, "
+        f"required_size={iban.country_specific_iban_size}",
+    )
 
 
 def raise_iban_does_not_match_country_format_error(iban: IbanModel):
     raise IbanDoesNotMatchCountryFormatError(
-        iban, "IBAN format is not valid for the specified country"
+        iban,
+        "IBAN format is not valid for the specified country, "
+        f"country={iban.country}, format={iban.iban_format}",
     )
