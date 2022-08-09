8c74712e20d46ed2a1ef9fc997cee103946ae41d
Ahmed Nafies <ahmed.nafies@gmail.com>
2022-08-09 13:48:23 +0200

    tests: adds more coverage for rules unit tests

--- app/rules/__init__.py
+++ app/rules/__init__.py
@@ -1,18 +1,13 @@
 from rules_engine import Otherwise, Rule, RulesEngine, then
 
 from app.rules.actions import (
-    raise_mod_97_check_error,
-    raise_not_alpha_numeric_error,
-    raise_iban_length_does_not_match_country_length_error,
     raise_iban_does_not_match_country_format_error,
-)
-
-from app.rules.conditions import (
-    mod_97_not_eq_to_one,
-    not_alpha_numeric,
-    iban_does_not_match_country_specific_length,
-    iban_does_not_match_country_format,
-)
+    raise_iban_length_does_not_match_country_length_error,
+    raise_mod_97_check_error, raise_not_alpha_numeric_error)
+from app.rules.conditions import (iban_does_not_match_country_format,
+                                  iban_does_not_match_country_specific_length,
+                                  mod_97_not_eq_to_one, not_alpha_numeric)
+from app.utils import trim
 
 
 def iban(iban: str) -> bool:
@@ -22,10 +17,10 @@ def iban(iban: str) -> bool:
             iban_does_not_match_country_specific_length,
             raise_iban_length_does_not_match_country_length_error,
         ),
-        Rule(mod_97_not_eq_to_one, raise_mod_97_check_error),
         Rule(
             iban_does_not_match_country_format,
             raise_iban_does_not_match_country_format_error,
         ),
+        Rule(mod_97_not_eq_to_one, raise_mod_97_check_error),
         Otherwise(then(True)),
-    ).run(iban)
+    ).run(trim(iban))
--- tests/unit/test_rules.py
+++ tests/unit/test_rules.py
@@ -1,6 +1,47 @@
-from app import rules, utils
+import pytest
+
+from app import rules
+from app.exceptions import (
+    IbanDoesNotMatchCountryFormatError,
+    IbanLengthError,
+    Mod97CheckError,
+    NotAlphaNumericError,
+)
 
 
 def test_iban_rules(valid_ibans):
     for iban in valid_ibans:
-        assert rules.iban(utils.trim(iban)) is True
+        assert rules.iban(iban) is True
+
+
+def test_invalid_iban_raises_length_error():
+    german_iban = "DE91100000000123456789"
+    assert rules.iban(german_iban) is True
+
+    invalid_german_iban = german_iban + "1"
+
+    with pytest.raises(IbanLengthError):
+        rules.iban(invalid_german_iban)
+
+    invalid_german_iban = german_iban[:-1]
+
+    with pytest.raises(IbanLengthError):
+        rules.iban(invalid_german_iban)
+
+
+def test_invalid_iban_raises_not_alpha_numeric_error():
+    invalid_iban = "DE91100000000#23456@89"
+    with pytest.raises(NotAlphaNumericError):
+        rules.iban(invalid_iban)
+
+
+def test_invalid_iban_raises_invalid_mod_97_operation_error():
+    invalid_iban = "DE91T00000000123456789"
+    with pytest.raises(IbanDoesNotMatchCountryFormatError):
+        rules.iban(invalid_iban)
+
+
+def test_invalid_iban_raises_invalid_country_format():
+    invalid_iban = "DET1100000000123456789"
+    with pytest.raises(Mod97CheckError):
+        rules.iban(invalid_iban)
