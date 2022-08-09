6db747a55c6b0d16f657a018cc3271669011d35f
Ahmed Nafies <ahmed.nafies@gmail.com>
2022-08-09 19:42:28 +0200

    test:  adds integration tests for endpoint validation and fixes error handlers

--- api/v1/errors.py
+++ api/v1/errors.py
@@ -3,8 +3,10 @@ from http import HTTPStatus
 from fastapi import Request
 from fastapi.responses import JSONResponse
 
-from app.exceptions import (CountryDoesNotExist, IbanLengthError,
-                            Mod97CheckError, NotAlphaNumericError)
+from app.exceptions import (CountryDoesNotExist,
+                            IbanDoesNotMatchCountryFormatError,
+                            IbanLengthError, Mod97CheckError,
+                            NotAlphaNumericError)
 
 
 def error_response(exc):
@@ -23,8 +25,10 @@ def error_response(exc):
 
 
 def register_errors(app):
-    @app.exception_handler(ValueError)
-    def value_error_exception_handler(request: Request, exc: ValueError):
+    @app.exception_handler(NotAlphaNumericError)
+    def not_alpha_numeric_exception_handler(
+        request: Request, exc: NotAlphaNumericError
+    ):
         return error_response(exc)
 
     @app.exception_handler(CountryDoesNotExist)
@@ -41,8 +45,8 @@ def register_errors(app):
     def iban_length_exception_handler(request: Request, exc: IbanLengthError):
         return error_response(exc)
 
-    @app.exception_handler(IbanLengthError)
-    def not_alpha_numeric_exception_handler(
-        request: Request, exc: NotAlphaNumericError
+    @app.exception_handler(IbanDoesNotMatchCountryFormatError)
+    def iban_country_format_exception_handler(
+        request: Request, exc: IbanDoesNotMatchCountryFormatError
     ):
         return error_response(exc)
--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -17,6 +17,38 @@ def test_iban_endpoint_returns_success_response_when_called_with_valid_ibans(
         }
 
 
+def test_iban_endpoint_returns_error_response_when_called_with_iban_is_gt_34(
+    api_client,
+):
+    iban = "GB82 WEST 1234 5698 7654 321 12312312346512341231412341241"
+    response = api_client.post("/api/v1/iban", json=dict(iban=iban))
+    response.status_code = 422
+    assert response.json() == {
+        "description": "IBAN Validation",
+        "content": {
+            "iban": "GB82WEST12345698765432112312312346512341231412341241",
+            "valid": False,
+            "message": "IBAN length=52, IBAN length cannot be greater than 34 characters",
+        },
+    }
+
+
+def test_iban_endpoint_returns_error_response_when_called_with_iban_is_lt_15(
+    api_client,
+):
+    iban = "GB82"
+    response = api_client.post("/api/v1/iban", json=dict(iban=iban))
+    response.status_code = 422
+    assert response.json() == {
+        "description": "IBAN Validation",
+        "content": {
+            "iban": "GB82",
+            "valid": False,
+            "message": "IBAN length cannot be less than 15 characters",
+        },
+    }
+
+
 def test_iban_endpoint_returns_error_response_when_called_with_iban_with_size_gt_required_size(
     api_client,
 ):
@@ -62,5 +94,53 @@ def test_iban_endpoint_returns_error_response_when_called_with_invalid_country_c
             "iban": "HM91100000000123456789",
             "valid": False,
             "message": "Country code HM is invalid",
+        },
+    }
+
+
+def test_iban_endpoint_returns_error_response_when_called_with_non_alphanumeric_iban(
+    api_client,
+):
+    iban = "GB82 WEST 1234 5698 7654 32&"
+    response = api_client.post("/api/v1/iban", json=dict(iban=iban))
+    response.status_code = 422
+    assert response.json() == {
+        "description": "IBAN Validation",
+        "content": {
+            "iban": "GB82WEST12345698765432&",
+            "valid": False,
+            "message": "The IBAN can contain only characters A-Z and 0-9",
+        },
+    }
+
+
+def test_iban_endpoint_returns_error_response_when_called_with_iban_invalid_country_format(
+    api_client,
+):
+    iban = "BI00 a234 5678 9123"
+    response = api_client.post("/api/v1/iban", json=dict(iban=iban))
+    response.status_code = 422
+    assert response.json() == {
+        "description": "IBAN Validation",
+        "content": {
+            "iban": "BI00a23456789123",
+            "valid": False,
+            "message": "IBAN format is not valid for the specified country, country=Burundi, format=BIkk nnnn nnnn nnnn",
+        },
+    }
+
+
+def test_iban_endpoint_returns_error_response_when_called_with_iban_invalid_mod_97_operation(
+    api_client,
+):
+    iban = "BI00 1234 5678 9123"
+    response = api_client.post("/api/v1/iban", json=dict(iban=iban))
+    response.status_code = 422
+    assert response.json() == {
+        "description": "IBAN Validation",
+        "content": {
+            "iban": "BI00123456789123",
+            "valid": False,
+            "message": "mod-97 operation as described in ISO 7064 has failed to validate the IBAN",
         },
     }
