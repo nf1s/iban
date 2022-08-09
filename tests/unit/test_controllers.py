6773cf1bf070169698af47d8fe0084ad0096ca88
Ahmed Nafies <ahmed.nafies@gmail.com>
2022-08-09 12:15:10 +0200

    fix: fixes broken tests due to broken import

--- tests/unit/test_controllers.py
+++ tests/unit/test_controllers.py
@@ -3,4 +3,4 @@ from app import controllers, utils
 
 def test_iban_rules(valid_ibans):
     for iban in valid_ibans:
-        assert controllers.validate_iban(utils.trim_iban(iban)) is True
+        assert controllers.validate_iban(utils.trim(iban)) is True
--- tests/unit/test_rules.py
+++ tests/unit/test_rules.py
@@ -3,4 +3,4 @@ from app import rules, utils
 
 def test_iban_rules(valid_ibans):
     for iban in valid_ibans:
-        assert rules.iban(utils.trim_iban(iban)) is True
+        assert rules.iban(utils.trim(iban)) is True
