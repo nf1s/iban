952254a86fa0edfb9fafb03af206baf048d5ad92
Ahmed Nafies <ahmed.nafies@gmail.com>
2022-08-09 20:16:48 +0200

    refactor: adds docstring

--- app/rules/__init__.py
+++ app/rules/__init__.py
@@ -1,26 +1,25 @@
-from rules_engine import Otherwise, Rule, RulesEngine, then
+from rules_engine import Otherwise, Rule, RulesEngine, not_, then
 
 from app.models import IbanModel
 from app.rules.actions import (
     raise_iban_does_not_match_country_format_error,
     raise_iban_length_does_not_match_country_length_error,
     raise_mod_97_check_error, raise_not_alpha_numeric_error)
-from app.rules.conditions import (iban_does_not_match_country_format,
-                                  iban_does_not_match_country_specific_length,
-                                  mod_97_not_eq_to_one, not_alpha_numeric)
+from app.rules.conditions import (alpha_numeric, matches_country_format,
+                                  matches_country_specific_length, mod_97)
 
 
 def iban(iban: IbanModel) -> bool:
     return RulesEngine(
-        Rule(not_alpha_numeric, raise_not_alpha_numeric_error),
+        Rule(not_(alpha_numeric), raise_not_alpha_numeric_error),
         Rule(
-            iban_does_not_match_country_specific_length,
+            not_(matches_country_specific_length),
             raise_iban_length_does_not_match_country_length_error,
         ),
         Rule(
-            iban_does_not_match_country_format,
+            not_(matches_country_format),
             raise_iban_does_not_match_country_format_error,
         ),
-        Rule(mod_97_not_eq_to_one, raise_mod_97_check_error),
+        Rule(not_(mod_97), raise_mod_97_check_error),
         Otherwise(then(True)),
     ).run(iban)
--- app/rules/actions.py
+++ app/rules/actions.py
@@ -5,14 +5,38 @@ from app.models import IbanModel
 
 
 def raise_not_alpha_numeric_error(iban: IbanModel):
+    """Function is called when IBAN is not alpha-numeric.
+
+    Args:
+        iban (IbanModel): iban
+
+    Raises:
+        exception: NotAlphaNumericError
+    """
     raise NotAlphaNumericError(iban)
 
 
 def raise_mod_97_check_error(iban: IbanModel):
+    """Function is called when IBAN mod 97 operation is not equal to 1.
+
+    Args:
+        iban (IbanModel): iban
+
+    Raises:
+        exception: Mod97CheckError
+    """
     raise Mod97CheckError(iban)
 
 
 def raise_iban_length_does_not_match_country_length_error(iban: IbanModel):
+    """Function is called when IBAN length does not match country specific length.
+
+    Args:
+        iban (IbanModel): iban
+
+    Raises:
+        exception: IbanLengthError
+    """
     raise IbanLengthError(
         iban,
         "IBAN length is not valid for the specified country, "
@@ -22,6 +46,14 @@ def raise_iban_length_does_not_match_country_length_error(iban: IbanModel):
 
 
 def raise_iban_does_not_match_country_format_error(iban: IbanModel):
+    """Method is called when IBAN format does not match the country.
+
+    Args:
+        iban (IbanModel): iban
+
+    Raises:
+        exception: IbanDoesNotMatchCountryFormatError
+    """
     raise IbanDoesNotMatchCountryFormatError(
         iban,
         "IBAN format is not valid for the specified country, "
